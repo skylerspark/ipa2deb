2f3ba6adc9e85864bc91e645be8796630edaf46b
Sir Code <taylorschneider07@yahoo.com>
2020-06-17 12:12:52 -0400

    First Changes

--- ipa2deb/Properties/AssemblyInfo.cs
+++ ipa2deb/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("viniciusjarina")]
+[assembly: AssemblyCopyright ("mistersircode")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
